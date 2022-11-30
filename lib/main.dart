c4927a635d477c3a5a98aed0ba07d9013bff6206
Adby Santos <adby.santos15@outlook.com>
2022-11-30 16:04:29 -0300

    feat: enabled option for device_preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'app/app_widget.dart';
 
 void main() {
-  runApp(DevicePreview(builder: (context) => const AppWidget()));
+  runApp(DevicePreview(enabled: kDebugMode, builder: (context) => const AppWidget()));
 }
 
 
